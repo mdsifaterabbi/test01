e26f22d404caaca3fb7d555f25118f027950e7ab
mdsifaterabbi <mdsifaterabbi641@gmail.com>
2024-03-10 11:40:21 +0600

    portfolio page er gallery large and extra large device done

---
+++ src/components/PortfolioComponents/PortfolioGallery.jsx
@@ -0,0 +1,55 @@
+import "./Gallery.css";
+
+const PortfolioGallery = () => {
+  return (
+    <>
+      <div className="flex flex-row flex-wrap">
+        <div className="basis-1/2">
+          <div className="flex flex-col">
+            <div className="basis-1/1">
+              <img
+                src="../../../../PortfolioImages/Noor_Co.png"
+                alt="Noor_Co"
+                className="border mx-auto w-[80%]"
+              ></img>
+            </div>
+            <div className="basis-1/1 mt-[20px]">
+              <img
+                src="../../../../PortfolioImages/Noor_Co_2.png"
+                alt="Noor_Co_2.png"
+                className="mx-auto border w-[80%]"
+              ></img>
+            </div>
+          </div>
+        </div>
+        <div className="basis-1/2">
+          <div className="flex flex-col">
+            <div className="basis-1/1 border-red-500 mb-[10px]">
+              <img
+                src="../../../../PortfolioImages/Noor_Co_3.png"
+                alt="Noor_Co_3"
+                className="border w-[80%]"
+              ></img>
+            </div>
+            <div className="basis-1/1 my-[10px]">
+              <img
+                src="../../../../PortfolioImages/Noor_Co_4.png"
+                alt="Noor_Co_4"
+                className="border w-[80%]"
+              ></img>
+            </div>
+            <div className="basis-1/1 my-[10px]">
+              <img
+                src="../../../../PortfolioImages/Noor_Co_5.png"
+                alt="Noor_Co_5"
+                className="border w-[80%]"
+              ></img>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default PortfolioGallery;
--- src/components/PortfolioComponents/PortfolioTabContents.jsx
+++ src/components/PortfolioComponents/PortfolioTabContents.jsx
@@ -3,6 +3,7 @@ import "react-tabs/style/react-tabs.css";
 
 import Slider from "react-slick";
 import { useState } from "react";
+import PortfolioGallery from "./PortfolioGallery";
 
 // import "../../MyTabOverlay.css";
 // import OrderForm from "../OrderForm";
@@ -83,34 +84,18 @@ const PortfolioTabContents = () => {
                     connections.
                   </p>
                 </div>
-                <div className="pb-[0px] xl:w-[70vw] xl:mx-auto">
-                  <div className="flex xl:flex-row flex-wrap">
-                    <div className="xl:basis-1/2 border">
-                      <div className="border bg-purple-500">Item 1</div>
-                    </div>
-                    <div className="xl:basis-1/2 border">
-                      <div className="border bg-purple-500">Item 2</div>
-                    </div>
-                    <div className="xl:basis-1/2 border">
-                      <div className="border bg-purple-500">Item 3</div>
-                    </div>
-                    <div className="xl:basis-1/2 border">
-                      <div className="border bg-purple-500">Item 4</div>
-                    </div>
-                    <div className="xl:basis-1/2 border">
-                      <div className="border bg-purple-500">Item 5</div>
-                    </div>
-                  </div>
+                <div className="pb-[0px] xl:w-[70vw] xl:mx-auto hidden lg:block xl:block">
+                  <PortfolioGallery />
                 </div>
               </TabPanel>
 
               {/* ============ Social Media Design Tab panel starts from here ================ */}
               <TabPanel className="xl:bg-[#ffffff]">
                 <div>
-                  <h1 className="text-center text-[#31b3fb] xl:text-[32px] xl:font-semibold">
+                  <h1 className="text-center text-[#31b3fb] lg:text-[26px] lg:font-semibold xl:text-[32px] xl:font-semibold">
                     Social Media Design
                   </h1>
-                  <p className="text-center xl:px-[300px] xl:pt-[10px] xl:pb-[30px] xl:font-semibold xl:text-[16px]">
+                  <p className="text-center lg:px-[150px] lg:pt-[10px] lg:pb-[30px] lg:font-semibold lg:text-[14px] xl:px-[300px] xl:pt-[10px] xl:pb-[30px] xl:font-semibold xl:text-[16px]">
                     At Esaviour Limited, we are more than just a digital
                     business development company; we are your dedicated ally in
                     navigating the dynamiclandscape of e-commerce and digital
@@ -119,10 +104,10 @@ const PortfolioTabContents = () => {
                     in the digital era.
                   </p>
                 </div>
-                <div className="slider-container p-0 bg-[#ffffff] xl:w-[70vw] xl:mx-auto">
+                <div className="slider-container p-0 bg-[#ffffff] lg:w-[90vw] xl:w-[70vw] lg:mx-auto xl:mx-auto">
                   <Slider {...settings}>
                     <div>
-                      <div className="card card-compact w-[15vw] bg-base-100  mx-auto shadow-xl h-auto">
+                      <div className="card card-compact lg:w-[20vw] xl:w-[15vw] bg-base-100  mx-auto shadow-xl h-auto">
                         <figure className="bg-[#ffffff] py-[0px]">
                           <img
                             src="../PortfolioImages/merry_christmas.png"
@@ -133,7 +118,7 @@ const PortfolioTabContents = () => {
                       </div>
                     </div>
                     <div>
-                      <div className="card card-compact w-[15vw] bg-base-100  mx-auto shadow-xl h-auto">
+                      <div className="card card-compact lg:w-[20vw] xl:w-[15vw] bg-base-100  mx-auto shadow-xl h-auto">
                         <figure className="bg-[#ffffff] py-[0px]">
                           <img
                             src="../PortfolioImages/happy_new_year.png"
@@ -144,7 +129,7 @@ const PortfolioTabContents = () => {
                       </div>
                     </div>
                     <div>
-                      <div className="card card-compact w-[15vw] bg-base-100  mx-auto shadow-xl h-auto">
+                      <div className="card card-compact lg:w-[20vw] xl:w-[15vw] bg-base-100  mx-auto shadow-xl h-auto">
                         <figure className="bg-[#ffffff] py-[0px]">
                           <img
                             src="../PortfolioImages/digital_marketing.png"
@@ -155,7 +140,7 @@ const PortfolioTabContents = () => {
                       </div>
                     </div>
                     <div>
-                      <div className="card card-compact w-[15vw] bg-base-100  mx-auto shadow-xl h-auto">
+                      <div className="card card-compact lg:w-[20vw] xl:w-[15vw] bg-base-100  mx-auto shadow-xl h-auto">
                         <figure className="bg-[#ffffff] py-[0px]">
                           <img
                             src="../PortfolioImages/build_your_brand.png"
@@ -171,10 +156,10 @@ const PortfolioTabContents = () => {
               {/* ============ Listing Images Design Tab panel starts from here ================ */}
               <TabPanel className="xl:bg-[#ffffff]">
                 <div>
-                  <h1 className="text-center text-[#31b3fb] xl:text-[32px] xl:font-semibold">
+                  <h1 className="text-center text-[#31b3fb] lg:text-[26px] lg:font-semibold xl:text-[32px] xl:font-semibold">
                     Amazon Listing Image Design
                   </h1>
-                  <p className="text-center xl:px-[300px] xl:pt-[10px] xl:pb-[30px] xl:font-semibold xl:text-[16px]">
+                  <p className="text-center lg:px-[150px] lg:pt-[10px] lg:pb-[30px] lg:font-semibold lg:text-[14px] xl:px-[300px] xl:pt-[10px] xl:pb-[30px] xl:font-semibold xl:text-[16px]">
                     At Esaviour Limited, we are more than just a digital
                     business development company; we are your dedicated ally in
                     navigating the dynamiclandscape of e-commerce and digital
@@ -186,45 +171,45 @@ const PortfolioTabContents = () => {
                 <div className="slider-container p-0  bg-[#ffffff] xl:w-[50vw] xl:mx-auto">
                   <Slider {...settings_third_panel}>
                     <div className="">
-                      <div className="card card-compact w-[50vw] bg-base-100 mx-auto shadow-xl xl:h-[500px]">
+                      <div className="card card-compact w-[50vw] xl:w-[50vw] lg:w-[90vw] bg-base-100 mx-auto shadow-xl lg:h-[450px] xl:h-[500px]">
                         <figure className="bg-[#f4f9fe] py-[0px]">
                           <img
                             src="../PortfolioImages/Amazon_1.jpeg"
                             alt="Amazon_1"
-                            className="xl:w-[100vw] xl:h-[500px]"
+                            className="xl:w-[100%] lg:w-[100%] lg:h-[450px] xl:h-[500px]"
                           ></img>
                         </figure>
                       </div>
                     </div>
                     <div>
-                      <div className="card card-compact w-[50vw] bg-base-100 mx-auto shadow-xl xl:h-[500px]">
+                      <div className="card card-compact w-[50vw] xl:w-[50vw] lg:w-[90vw] bg-base-100 mx-auto shadow-xl lg:h-[450px] xl:h-[500px]">
                         <figure className="bg-[#f4f9fe] py-[0px]">
                           <img
                             src="../PortfolioImages/Amazon_2.jpeg"
                             alt="Amazon_2"
-                            className="xl:w-[100vw] xl:h-[500px]"
+                            className="xl:w-[100%] lg:w-[100%] lg:h-[450px] xl:h-[500px]"
                           ></img>
                         </figure>
                       </div>
                     </div>
                     <div>
-                      <div className="card card-compact w-[50vw] bg-base-100 mx-auto shadow-xl xl:h-[500px]">
+                      <div className="card card-compact w-[50vw] xl:w-[50vw] lg:w-[90vw] bg-base-100 mx-auto shadow-xl lg:h-[450px] xl:h-[500px]">
                         <figure className="bg-[#f4f9fe] py-[0px]">
                           <img
                             src="../PortfolioImages/Amazon_3.jpeg"
                             alt="Amazon_3"
-                            className="xl:w-[100vw] xl:h-[500px]"
+                            className="xl:w-[100%] lg:w-[100%] lg:h-[450px] xl:h-[500px]"
                           ></img>
                         </figure>
                       </div>
                     </div>
                     <div>
-                      <div className="card card-compact w-[50vw] bg-base-100 mx-auto shadow-xl xl:h-[500px]">
+                      <div className="card card-compact w-[50vw] xl:w-[50vw] lg:w-[90vw] bg-base-100 mx-auto shadow-xl lg:h-[450px] xl:h-[500px]">
                         <figure className="bg-[#f4f9fe] py-[0px]">
                           <img
                             src="../PortfolioImages/Amazon_4.jpeg"
                             alt="Amazon_4"
-                            className="xl:w-[100vw] xl:h-[500px]"
+                            className="xl:w-[100%] lg:w-[100%] lg:h-[450px] xl:h-[500px]"
                           ></img>
                         </figure>
                       </div>
@@ -235,10 +220,10 @@ const PortfolioTabContents = () => {
               {/* ============ Amazon A+ Content Tab panel starts from here ================ */}
               <TabPanel className="xl:bg-[#ffffff]">
                 <div>
-                  <h1 className="text-center text-[#31b3fb] xl:text-[32px] xl:font-semibold">
+                  <h1 className="text-center text-[#31b3fb] lg:text-[26px] lg:font-semibold xl:text-[32px] xl:font-semibold">
                     Amazon EBC Design
                   </h1>
-                  <p className="text-center xl:px-[300px] xl:pt-[10px] xl:pb-[30px] xl:font-semibold xl:text-[16px]">
+                  <p className="text-center lg:px-[150px] lg:pt-[10px] lg:pb-[30px] lg:font-semibold lg:text-[14px] xl:px-[300px] xl:pt-[10px] xl:pb-[30px] xl:font-semibold xl:text-[16px]">
                     At Esaviour Limited, we are more than just a digital
                     business development company; we are your dedicated ally in
                     navigating the dynamic landscape of e-commerce and
@@ -247,10 +232,10 @@ const PortfolioTabContents = () => {
                     businesses to thrive in the digital era.
                   </p>
                 </div>
-                <div className="slider-container p-0  bg-[#ffffff] xl:w-[40vw] xl:mx-auto">
+                <div className="slider-container p-0  bg-[#ffffff] lg:w-[70vw] lg:mx-auto xl:w-[40vw] xl:mx-auto">
                   <Slider {...settings_fourth_panel}>
                     <div>
-                      <div className="card card-compact w-[20vw] bg-base-100 mx-auto">
+                      <div className="card card-compact lg:w-[35vw] xl:w-[20vw] bg-base-100 mx-auto">
                         <figure className="bg-[#ffffff] py-[0px]">
                           <img
                             src="../PortfolioImages/amazon_ap_1.png"
@@ -261,7 +246,7 @@ const PortfolioTabContents = () => {
                       </div>
                     </div>
                     <div>
-                      <div className="card card-compact w-[20vw] bg-base-100 mx-auto ">
+                      <div className="card card-compact lg:w-[35vw] xl:w-[20vw] bg-base-100 mx-auto ">
                         <figure className="bg-[#ffffff] py-[0px]">
                           <img
                             src="../PortfolioImages/amazon_ap_2.png"
@@ -272,7 +257,7 @@ const PortfolioTabContents = () => {
                       </div>
                     </div>
                     <div>
-                      <div className="card card-compact w-[20vw] bg-base-100 mx-auto">
+                      <div className="card card-compact lg:w-[35vw] xl:w-[20vw] bg-base-100 mx-auto">
                         <figure className="bg-[#ffffff] py-[0px]">
                           <img
                             src="../PortfolioImages/amazon_ap_3.png"
@@ -288,10 +273,10 @@ const PortfolioTabContents = () => {
               {/* ============ Product Package Design Tab panel starts from here ================ */}
               <TabPanel className="xl:bg-[#ffffff]">
                 <div>
-                  <h1 className="text-center text-[#31b3fb] xl:text-[32px] xl:font-semibold">
-                    Product packaging Desig
+                  <h1 className="text-center text-[#31b3fb] lg:text-[24px] lg:font-semibold xl:text-[32px] xl:font-semibold">
+                    Product packaging Design
                   </h1>
-                  <p className="text-center xl:px-[300px] xl:pt-[10px] xl:pb-[30px] xl:font-semibold xl:text-[16px]">
+                  <p className="text-center lg:px-[100px] lg:pt-[10px] lg:pb-[25px] lg:font-semibold lg:text-[14px] xl:px-[300px] xl:pt-[10px] xl:pb-[30px] xl:font-semibold xl:text-[16px]">
                     At Esaviour Limited, we are more than just a digital
                     business development company; we are your dedicated ally in
                     navigating the dynamic landscape of e-commerce and digital
@@ -300,14 +285,14 @@ const PortfolioTabContents = () => {
                     in the digital era.
                   </p>
                 </div>
-                <div className="p-0  xl:w-[70vw] xl:mx-auto bg-[#ffffff]">
+                <div className="p-0 lg:w-[95vw] lg:mx-auto xl:w-[70vw] xl:mx-auto bg-[#ffffff]">
                   <div>
-                    <div className="card card-compact w-[70vw] bg-base-100 mx-auto">
+                    <div className="card card-compact lg:w-[95vw] xl:w-[70vw] bg-base-100 mx-auto">
                       <figure className="bg-[#ffffff] py-[0px]">
                         <img
                           src="../PortfolioImages/product_package_design.png"
                           alt="product_package_design.png"
-                          className="w-[70%] mx-auto"
+                          className="lg:w-[80%] xl:w-[70%] mx-auto"
                         ></img>
                       </figure>
                     </div>
@@ -317,10 +302,10 @@ const PortfolioTabContents = () => {
               {/* ============ webdevelopment Tab panel starts from here ================ */}
               <TabPanel className="xl:bg-[#ffffff]">
                 <div>
-                  <h1 className="text-center text-[#31b3fb] xl:text-[32px] xl:font-semibold">
+                  <h1 className="text-center text-[#31b3fb] lg:text-[26px] lg:font-semibold xl:text-[32px] xl:font-semibold">
                     Web Development
                   </h1>
-                  <p className="text-center xl:px-[300px] xl:pt-[10px] xl:pb-[30px] xl:font-semibold xl:text-[16px]">
+                  <p className="text-center lg:px-[100px] lg:pt-[10px] lg:pb-[25px] lg:font-semibold lg:text-[14px] xl:px-[300px] xl:pt-[10px] xl:pb-[30px] xl:font-semibold xl:text-[16px]">
                     At Esaviour Limited, we are more than just a digital
                     business development company; we are your dedicated ally in
                     navigating the dynamic landscape of e-commerce and
@@ -332,34 +317,34 @@ const PortfolioTabContents = () => {
                 <div className="slider-container p-0  bg-[#ffffff] xl:w-[40vw] xl:mx-auto">
                   <Slider {...settings_web_panel}>
                     <div>
-                      <div className="card card-compact w-[20w] bg-base-100 mx-auto">
+                      <div className="card card-compact lg:w-[40vw] xl:w-[20vw] bg-base-100 mx-auto">
                         <figure className="bg-[#ffffff] py-[0px]">
                           <img
                             src="../PortfolioImages/web1.png"
                             alt="web1"
-                            className="w-[90%] xl:h-[550px]"
+                            className="w-[90%] lg:h-[500px] xl:h-[550px]"
                           ></img>
                         </figure>
                       </div>
                     </div>
                     <div>
-                      <div className="card card-compact w-[20vw] mx-auto bg-base-100">
+                      <div className="card card-compact lg:w-[40vw] xl:w-[20vw] mx-auto bg-base-100">
                         <figure className="bg-[#ffffff] py-[0px]">
                           <img
                             src="../PortfolioImages/web2.png"
                             alt="web2"
-                            className="w-[90%] xl:h-[550px]"
+                            className="w-[90%] lg:h-[500px] xl:h-[550px]"
                           ></img>
                         </figure>
                       </div>
                     </div>
                     <div>
-                      <div className="card card-compact w-[20vw] mx-auto bg-base-100">
+                      <div className="card card-compact lg:border lg:w-[40vw] xl:w-[20vw] mx-auto bg-base-100">
                         <figure className="bg-[#ffffff] py-[0px]">
                           <img
                             src="../PortfolioImages/web3.png"
                             alt="../PortfolioImages/web3"
-                            className="w-full xl:h-[550px]"
+                            className="w-[90%] lg:h-[500px] xl:h-[550px]"
                           ></img>
                         </figure>
                       </div>
