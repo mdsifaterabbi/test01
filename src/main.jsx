2a5d05496a85e0eb723052b957c3bb00f5fb0ccc
mdsifaterabbi <mdsifaterabbi641@gmail.com>
2024-03-15 16:28:25 +0600

    email js was successfully implemented for Get a Quote button in hero section

--- src/main.jsx
+++ src/main.jsx
@@ -4,13 +4,16 @@ import App from "./App.jsx";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
 import { HelmetProvider } from "react-helmet-async";
+import { EmailJSProvider } from "./ContextAPI/EmailJSContextAPI.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <HelmetProvider>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </HelmetProvider>
+    <EmailJSProvider>
+      <HelmetProvider>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </HelmetProvider>
+    </EmailJSProvider>
   </React.StrictMode>
 );
